054338400ce0337e5ac255ed5b4c7556d5ea40ef
yuryklim <yuryklim@outlook.com>
2019-01-19 22:56:07 +0200

    creating a filter pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {ShortenPipe} from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent, ShortenPipe
+    AppComponent, ShortenPipe, FilterPipe
   ],
   imports: [
     BrowserModule,
